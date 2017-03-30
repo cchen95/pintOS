03cbe0ea9f1cefaf7911484424dc549b005145a1
Christopher Chen <christopher.chen95@berkeley.edu>
2017-03-30 20:23:09 +0000

    cc - full revert of synch files

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -22,14 +22,11 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-    int priority_lock;
-    struct list_elem elem_lock;
   };
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
-struct thread *get_waiter_with_highest_priority(struct semaphore *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
