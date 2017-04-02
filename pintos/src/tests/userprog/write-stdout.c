92b46980ae899238ab6ecadc2e0a4b0c8d866de8
solahyoo <tjf009@berkeley.edu>
2017-04-02 21:34:49 +0000

    sy - added write-stdout test

---
+++ pintos/src/tests/userprog/write-stdout.c
@@ -0,0 +1,19 @@
+/* Write to fd 1 (stdout). */
+
+#include <syscall.h>
+#include "tests/userprog/sample.inc"
+#include "tests/lib.h"
+#include "tests/main.h"
+
+void
+test_main (void)
+{
+  int handle, byte_cnt;
+
+  CHECK (create ("test.txt", sizeof sample - 1), "create \"test.txt\"");
+  CHECK ((handle = open ("test.txt")) > 1, "open \"test.txt\"");
+
+  byte_cnt = write (1, sample, sizeof sample - 1);
+  if (byte_cnt != sizeof sample - 1)
+    fail ("write() returned %d instead of %zu", byte_cnt, sizeof sample - 1);
+}
---
+++ pintos/src/tests/userprog/write-stdout.ck
@@ -0,0 +1,16 @@
+# -*- perl -*-
+use strict;
+use warnings;
+use tests::tests;
+check_expected ([<<'EOF']);
+(write-stdout) begin
+(write-stdout) create "test.txt"
+(write-stdout) open "test.txt"
+"Amazing Electronic Fact: If you scuffed your feet long enough without
+ touching anything, you would build up so many electrons that your
+ finger would explode!  But this is nothing to worry about unless you
+ have carpeting." --Dave Barry
+(write-stdout) end
+write-stdout: exit(0)
+EOF
+pass;
